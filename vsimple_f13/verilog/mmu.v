1050e180acd186a6fba04bad20bb34b3b775b21e
Junyi Zhang <zhangjy@caen-vnc04.engin.umich.edu>
2013-11-17 14:44:42 -0500

    upload mmu.v Junyi

---
+++ vsimple_f13/verilog/mmu.v
@@ -0,0 +1,25 @@
+`define lower_bound	8000
+`define upper_bound	8800
+
+module mmu ( // Inputs
+		proc2mem_addr,
+		login,
+		trigger,	
+
+             // Outputs
+		mem_addr,
+		protected
+           );
+
+input	[63:0]		proc2mem_addr;
+input			login;
+input			trigger;
+
+output	[63:0]		mem_addr;
+output			protected;
+
+assign	protected 	= (login || trigger) 	? 0
+						: ( proc2mem_addr >= `lower_bound && proc2mem_addr <= `upper_bound ) ? 1 : 0;
+assign	mem_addr	= protected ? 0 : proc2mem_addr;
+
+endmodule
